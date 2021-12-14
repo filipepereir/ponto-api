d780b26630bcf37f803c0ce2af4ff204d486019e
filipe.jesus <33381421+filipepereir@users.noreply.github.com>
2021-12-14 18:36:35 -0300

    initial commit

---
+++ src/test/java/com/ponto/api/PontoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
