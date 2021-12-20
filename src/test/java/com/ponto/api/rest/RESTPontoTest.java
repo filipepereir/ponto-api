1c23d93186875cd3a10847f8e0dd699a2d1566d1
filipe.jesus <33381421+filipepereir@users.noreply.github.com>
2021-12-20 15:04:10 -0300

    Testes de integração RESTPonto
    Testes de integração RESTPonto

--- src/test/java/com/ponto/api/rest/RESTPontoTest.java
+++ src/test/java/com/ponto/api/rest/RESTPontoTest.java
@@ -1,6 +1,10 @@
 package com.ponto.api.rest;
 
+import com.ponto.api.entity.dto.LocalizacaoDTO;
 import com.ponto.api.rest.utils.RESTIntegracao;
+import io.restassured.http.ContentType;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,17 +16,24 @@ import static org.junit.jupiter.api.Assertions.fail;
 @SpringBootTest
 class RESTPontoTest extends RESTIntegracao {
 
-    private static final String BASE_URL = "http://localhost:8080/ponto/registro/v1";
-
-
+    private static final String BASE_URL = "http://localhost:8080/ponto/registro";
 
 
     @Test
-    void testRegistrarPonto() {
+    void testRegistrarPonto() throws JSONException {
+
+        JSONObject jsonRegistrarPonto = new JSONObject();
+        Double latitude = 3151515151D;
+        Double longitude = -21516164945D;
+        jsonRegistrarPonto.put("latitude", latitude);
+        jsonRegistrarPonto.put("longitude", longitude);
+
         given()
                 .when()
                 .headers("Authorization", "Bearer " + token)
-                .get(BASE_URL)
+                .contentType(ContentType.JSON)
+                .body(jsonRegistrarPonto.toString())
+                .post(BASE_URL)
                 .then()
                 .statusCode(200)
                 .log()
@@ -31,12 +42,26 @@ class RESTPontoTest extends RESTIntegracao {
 
     @Test
     void testBuscarRegistrosV1() {
-        fail("Not yet implemented");
+        given()
+                .when()
+                .headers("Authorization", "Bearer " + token)
+                .get(BASE_URL + "/v1")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
     @Test
     void testBuscarRegistrosV2() {
-        fail("Not yet implemented");
+        given()
+                .when()
+                .headers("Authorization", "Bearer " + token)
+                .get(BASE_URL + "/v2")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
 }
